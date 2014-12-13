ff8ece6d20ba619e0ded301d993baf38022539ef
jimmay5469 <jimmay5469@gmail.com>
2014-12-12 22:44:20 -0500

    Grouping sessions times by day

--- app/controllers/plan.js
+++ app/controllers/plan.js
@@ -2,5 +2,19 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   sessionTimes: Ember.computed.map('model', function(item) { return item.SessionTime; }),
-  uniqueSessionTimes: Ember.computed.uniq('sessionTimes')
+  uniqueSessionTimes: Ember.computed.uniq('sessionTimes'),
+  groupedSessionTimes: function() {
+    var groupObj = this.get('uniqueSessionTimes').reduce(function(grouping,item) {
+      var day = moment(item).startOf('day');
+      if (grouping[day]) {
+        grouping[day].push(item);
+      } else {
+        grouping[day] = [item];
+      }
+      return grouping;
+    }, {});
+    return Object.keys(groupObj).map(function(key) {
+      return { key: key, values: groupObj[key] };
+    });
+  }.property('uniqueSessionTimes')
 });
