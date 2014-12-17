97d61671e8a906d74d7fc438a6a7ccc52a37fff7
jimmay5469 <jimmay5469@gmail.com>
2014-12-17 14:11:36 -0500

    Don't proxy api because it has CORS

--- app/routes/plan.js
+++ app/routes/plan.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      Ember.$.getJSON('/api/sessionsdata').done(resolve).fail(reject);
+      Ember.$.getJSON('https://cmprod-speakers.azurewebsites.net/api/sessionsdata').done(resolve).fail(reject);
     });
   }
 });
