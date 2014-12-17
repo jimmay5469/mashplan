1b187fc8e845ab57cfc4a2b3820d35fc4bbdcb10
jimmay5469 <jimmay5469@gmail.com>
2014-12-17 14:47:04 -0500

    Add root for gh-pages

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'mashplan',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/mashplan/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
