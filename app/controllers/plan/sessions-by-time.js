70bb0c978af04621daa34a62e49204f16db92822
jimmay5469 <jimmay5469@gmail.com>
2014-12-31 02:21:37 -0500

    Layout and styling

--- app/controllers/plan/sessions-by-time.js
+++ app/controllers/plan/sessions-by-time.js
@@ -1,3 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.ArrayController.extend();
+export default Ember.ArrayController.extend({
+  SessionStartTime: function() {
+    var model = this.get('model');
+    if (!model.length) {
+      return null;
+    }
+    return model[0].SessionStartTime;
+  }.property('model')
+});
