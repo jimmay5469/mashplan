eb6f019b16d79d73f156f2b498997fe191582e71
jimmay5469 <jimmay5469@gmail.com>
2014-12-31 01:03:39 -0500

    Filter out Kidz Mash

--- app/routes/plan.js
+++ app/routes/plan.js
@@ -4,6 +4,11 @@ export default Ember.Route.extend({
   model: function() {
     return new Ember.RSVP.Promise(function(resolve, reject) {
       Ember.$.getJSON('https://cmprod-speakers.azurewebsites.net/api/sessionsdata').done(resolve).fail(reject);
+    })
+    .then(function(data) {
+      return data.filter(function(item) {
+        return item.SessionType !== 'Kidz Mash';
+      });
     });
   }
 });
