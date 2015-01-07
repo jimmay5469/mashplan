17e317e18cb6054c3f0eed4cccdb2480431dd66f
jimmay5469 <jimmay5469@gmail.com>
2015-01-06 22:38:05 -0500

    Add refresh button

--- app/routes/plan.js
+++ app/routes/plan.js
@@ -3,12 +3,26 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      Ember.$.getJSON('https://cmprod-speakers.azurewebsites.net/api/sessionsdata').done(resolve).fail(reject);
+      if(localStorage.getItem('sessions')) {
+        resolve(JSON.parse(localStorage.getItem('sessions')));
+      } else {
+        Ember.$.getJSON('https://cmprod-speakers.azurewebsites.net/api/sessionsdata').done(resolve).fail(reject);
+      }
+    })
+    .then(function(data) {
+      localStorage.setItem('sessions', JSON.stringify(data));
+      return data;
     })
     .then(function(data) {
       return data.filter(function(item) {
         return item.SessionType !== 'Kidz Mash' && item.SessionType !== 'CodeMash Schedule Item';
       });
     });
+  },
+  actions: {
+    refreshSessions: function() {
+      localStorage.removeItem('sessions');
+      this.refresh();
+    }
   }
 });
